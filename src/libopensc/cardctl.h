6778e9d0e75babe5a69b0dff752fe56320009e7e
Luka Logar <luka.logar@cifra.si>
2021-06-08 10:28:08 +0200

    IsoApplet: add support for arbitrary length RSA

--- src/libopensc/cardctl.h
+++ src/libopensc/cardctl.h
@@ -1053,7 +1053,7 @@ typedef struct sc_cardctl_sc_hsm_wrapped_key {
  * isoApplet
  */
 
-#define SC_ISOAPPLET_ALG_REF_RSA_GEN_2048 0xF3
+#define SC_ISOAPPLET_ALG_REF_RSA_GEN 0xF3
 #define SC_ISOAPPLET_ALG_REF_EC_GEN 0xEC
 
 typedef struct sc_cardctl_isoApplet_ec_parameters {
@@ -1068,6 +1068,7 @@ typedef struct sc_cardctl_isoApplet_ec_parameters {
 typedef struct sc_cardctl_isoApplet_genkey {
 	u8 algorithm_ref;			/* Algorithm reference sent to card */
 	unsigned int priv_key_ref;	/* Private key reference sent to card */
+	size_t key_len;
 	union {
 		struct
 		{
@@ -1085,6 +1086,7 @@ typedef struct sc_cardctl_isoApplet_genkey {
 typedef struct sc_cardctl_isoApplet_import_key {
 	u8 algorithm_ref;			/* Algorithm reference sent to card */
 	unsigned int priv_key_ref;	/* Private key reference sent to card */
+	size_t key_len;
 	union {
 		struct
 		{
